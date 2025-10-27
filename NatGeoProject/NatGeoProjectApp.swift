8bef519ad5147efb01092b91244df92794552449
Mami Ravaloarison <maminiainaravaloarison@gmail.com>
2025-10-27 14:59:39 -0400

    Initial Commit

---
+++ NatGeoProject/NatGeoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatGeoProjectApp.swift
+//  NatGeoProject
+//
+//  Created by Mami RavaLoarison on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct NatGeoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
